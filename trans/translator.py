48b4ff9a5ce13fc06cf01240a2d30b8843affff3
Antti Kuparinen <akuparinen@iki.fi>
2023-09-13 14:24:49 +0300

    Komentorivikäyttöliittymä testausta varten

--- trans/translator.py
+++ trans/translator.py
@@ -1,8 +1,25 @@
+print("Ladataan malli...", end="", flush=True)
+
 from transformers import pipeline
 
 pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-fi")
+print(" Valmis")
 
 
 def translate(text):
-    result = pipe(text)
-    return result[0]['translation_text']
+    result = pipe(text.split("\n\n"))
+    return [ block['translation_text'] for block in result ]
+
+
+if __name__ == "__main__":
+    import sys
+
+    print("\b"*len("Ladataan...") + "Anna teksti ja paina Ctrl-D: ")
+    text = sys.stdin.read()
+
+    print("━" * 80)
+    blocks = translate(text)
+    for block in blocks:
+        print(block)
+        if block != blocks[-1]:
+            print()
